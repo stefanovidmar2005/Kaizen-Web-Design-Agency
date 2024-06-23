0d5e4590b946a3e0d2a504f8cb9d9206085fd9a5
stefanovidmar2005 <stefanovidmar@gmail.com>
2024-06-23 13:28:42 +0300

    configured Animate on Scroll

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import icon from "astro-icon";
 import { register } from "swiper/element/bundle";
+import AOS from "aos";
 register();
+AOS.init();
 export default defineConfig({
   integrations: [icon()],
   image: {
